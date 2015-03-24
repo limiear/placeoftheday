e7ac7f21ed59c07be248db94de443302faab18de
eloy <eloy.colell@gmail.com>
2015-03-24 16:20:03 -0300

    It reduce the google image size.

--- placeoftheday/grapher.py
+++ placeoftheday/grapher.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from placedescriptor import get
 import urllib
 import json
+from PIL import Image
 
 
 def download(source, destiny):
@@ -18,14 +19,19 @@ def draw(history, latlon, name):
                 "&zoom=14&size=200x200&maptype=satellite&sensor=false"
                 "&markers=size:mid|color:red|label:P|%s" % (latlon, latlon))
     download(city_map, 'sat_map.png')
+    result = ['map.png', 'sat_map.png']
     url = ("https://ajax.googleapis.com/ajax/services/search/"
            "images?v=1.0&q=%s photographs" % name)
     data = json.load(urllib.urlopen(url))
-    result = ['map.png', 'sat_map.png']
     images = map(lambda r: r['url'], data['responseData']['results'])
     images = filter(lambda f: f[-3:] in ['png', 'jpg', 'gif', 'bmp'], images)
     if len(images):
         picture = 'picture.%s' % images[0][-3:]
         download(images[0], picture)
+        im = Image.open(picture)
+        size = im.size
+        ratio = size[1] / float(size[0])
+        im.resize((400, int(400 * ratio)), Image.BILINEAR)
+        im.save(picture)
         result.append(picture)
     return result
