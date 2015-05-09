6e71025ae6aa0b16e4945f15c51d83b057fb1cd6
eloy <eloy.colell@gmail.com>
2015-05-09 19:20:36 -0300

    It try to fix a bug.

--- placeoftheday/followers_bot.py
+++ placeoftheday/followers_bot.py
@@ -291,10 +291,10 @@ def strategy():
                           text='%i new followers (%i).' % (news, len(end)))
     print "Projecting next week..."
     huge_group = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
-    huge_group = huge_group if huge_group > 1000 else 1000
     auto_unfollow_nonfollowers()
     better_group = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
-    posible_group = (len(huge_group) - len(better_group)) * 1.01
+    posible_group = len(huge_group) if len(huge_group) > 1000 else 1000
+    posible_group = (posible_group - len(better_group)) * 1.01
     print "Selecting trends..."
     countries = ['Worldwide', 'Argentina', 'Brazil', 'Chile', 'Spain']
     """
